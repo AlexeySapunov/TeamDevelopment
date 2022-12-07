3bb7bca2e09921199ca7a183637a58e9c22188a7
Alexey <blacka8607@rambler.ru>
2022-12-07 15:52:56 +0300

    Added UserService

--- admin/src/main/java/ru/gb/service/UserServiceImpl.java
+++ admin/src/main/java/ru/gb/service/UserServiceImpl.java
@@ -1,4 +1,108 @@
 package ru.gb.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.gb.database.model.User;
+import ru.gb.database.repo.RoleRepository;
+import ru.gb.database.repo.UserRepository;
+import ru.gb.database.repo.UserSpecification;
+import ru.gb.controler.dto.RoleDto;
+import ru.gb.controler.dto.UserDto;
+import ru.gb.controler.dto.UserListParams;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public List<UserDto> findAll() {
+        return userRepository.findAll().stream()
+                .map(user -> new UserDto(
+                        user.getId(),
+                        user.getUsername()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<UserDto> findWithFilter(UserListParams userListParams) {
+        Specification<User> spec = Specification.where(null);
+
+        if (userListParams.getUsernameFilter() != null && !userListParams.getUsernameFilter().isBlank()) {
+            spec = spec.and(UserSpecification.nameLike(userListParams.getUsernameFilter()));
+        }
+        if (userListParams.getMinAge() != null) {
+            spec = spec.and(UserSpecification.minAge(userListParams.getMinAge()));
+        }
+        if (userListParams.getMaxAge() != null) {
+            spec = spec.and(UserSpecification.maxAge(userListParams.getMaxAge()));
+        }
+
+        return userRepository.findAll(spec,
+                        PageRequest.of(
+                                Optional.ofNullable(userListParams.getPage()).orElse(1) - 1,
+                                Optional.ofNullable(userListParams.getSize()).orElse(3),
+                                Sort.by(Optional.ofNullable(userListParams.getSortField())
+                                        .filter(c -> !c.isBlank())
+                                        .orElse("id"))))
+                .map(user -> new UserDto(user.getId(), user.getUsername()));
+    }
+
+    @Override
+    public Optional<UserDto> findById(Long id) {
+        return userRepository.findById(id)
+                .map(user -> new UserDto(
+                        user.getId(),
+                        user.getUsername(),
+                        user.getEmail(),
+                        mapRolesDto(user)));
+    }
+
+    @Override
+    public void save(UserDto userDto) {
+        User user = new User(
+                userDto.getId(),
+                userDto.getUsername(),
+                userDto.getEmail(),
+                passwordEncoder.encode(userDto.getPassword()),
+                userDto.getRoles()
+                        .stream()
+                        .map(roleDto -> roleRepository.getReferenceById(roleDto.getId()))
+                        .collect(Collectors.toSet()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    private static Set<RoleDto> mapRolesDto(User user) {
+        return user.getRoles()
+                .stream()
+                .map(role -> new RoleDto(role.getId(), role.getName()))
+                .collect(Collectors.toSet());
+    }
 }
